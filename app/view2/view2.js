b9793b6ba08e65ec08bd95569c0e0cf5650747a9
joezhouoxford <joezhou@spengineeringltd.com>
2016-05-10 07:10:57 +0100

    show in grid

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,7 +9,61 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope',function($scope) {
+.controller('View2Ctrl', ['$scope','$q',function($scope,$q) {
+  function retrieveListItems(){
+    var listTitle="test";
+    var queryString="$select=Title,Employee,Company";
+    var endArray=[];
+    getListItemViaREST("testlist1","$select=Title,Employee,Company").then(
+        function(data){
+          consolidateListData(endArray,data);
+          return getListItemViaREST("testlist2","$select=Title,Employee,Company");
+        }
+    ).then(
+        function(data){
+          consolidateListData(endArray,data);
+          return getListItemViaREST("testlist3","$select=Title,Employee,Company");
+        }
+    ).then(
+        function(data){
+          consolidateListData(endArray,data);
+        }
+    ).catch(function(data){cosole.error(data);alert("retrieve list item failed");});
+
+
+  }
+
+
+  function consolidateListData(endArray,listItems)
+  {
+
+    return endArray;
+  }
+
+  function getListItemViaREST(listTitle,queryString){
+    var deferred = $q.defer();
+    $http(
+        {
+          method: 'GET',
+          url: _spPageContextInfo.webAbsoluteUrl +
+          "/_api/web/lists/getByTitle('"+listTitle+"')/items?"+queryString,
+          headers:
+          {
+            "Accept": "application/json;odata=verbose"
+          }
+        }).success(function(data, status,
+                            headers, config)
+    {
+
+      deferred.resolve(data.d.results);
+    }).error(function(data, status,
+                      headers, config) {
+      deferred.reject(data);
+    });
+
+    return deferred.promise;
+  }
+
   var myData = [
     {
      "firstName": "Cox",
