69a60ca828a712c5d3298f5d218a429d8b865caa
joezhouoxford <joezhou@spengineeringltd.com>
2016-05-11 06:11:21 +0100

    show in grid

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version',
     'ui.grid'
-]).
+])/*.
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])*/;
